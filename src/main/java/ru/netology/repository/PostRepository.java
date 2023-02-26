5f3af00081f8870d4ddaabedaf0435fcbd3ac701
RiZaRtlt <35322018+RiZaRtlt@users.noreply.github.com>
2023-02-26 19:26:54 +0400

    Update PostRepository.java

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -3,11 +3,13 @@ package ru.netology.repository;
 import ru.netology.model.Post;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 
 // Stub
 public class PostRepository {
-  protected Map<Long, Post> map = new HashMap<>();
-  protected long globalId = 0;
+  protected Map<Long, Post> map = new ConcurrentHashMap<>();
+  protected AtomicLong globalId = new AtomicLong(0);
 
   public List<Post> all() {
     List<Post> list = new ArrayList<>(map.values());
@@ -23,11 +25,11 @@ public class PostRepository {
     long id = post.getId();
 
     if (id==0) {
-      globalId++;
-      id = globalId;
+      globalId.incrementAndGet();
+      id = globalId.get();
       post.setId(id);
     } else {
-      if (id > globalId) {globalId = id;}
+      if (id > globalId.get()) {globalId.set(id);}
     }
     map.put(id, post);
 
